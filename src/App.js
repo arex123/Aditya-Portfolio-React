d907e07326b4f25f7c2d8d34e35822a5d4141d23
Aditya <ad47kumar@gmail.com>
2023-02-11 15:39:24 +0530

    initial setup done and readme changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import { BrowserRouter,Router,Route } from 'react-router-dom';
+import Experience from './Pages/Experience';
+import Landing from './Pages/Landing'
+import Project from './Pages/Projects';
 
 function App() {
   return (
     <div className="App">
+
+      <BrowserRouter>
+        <Router>
+          <Route path='/' element={<Landing/>}/>
+          <Route path='/experience' element={<Experience/>} />
+          <Route path='/project' element={<Project/>} />
+        </Router>
+      </BrowserRouter>
 
       
     </div>
