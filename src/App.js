45362429ebfc27448558c57d03f63e0c18c2fa5d
Aditya <ad47kumar@gmail.com>
2023-02-11 15:52:50 +0530

    error fixed which was due to missed routes module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter,Router,Route } from 'react-router-dom';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Experience from './Pages/Experience';
 import Landing from './Pages/Landing'
 import Project from './Pages/Projects';
@@ -9,11 +9,11 @@ function App() {
     <div className="App">
 
       <BrowserRouter>
-        <Router>
+        <Routes>
           <Route path='/' element={<Landing/>}/>
           <Route path='/experience' element={<Experience/>} />
           <Route path='/project' element={<Project/>} />
-        </Router>
+        </Routes>
       </BrowserRouter>
 
       
