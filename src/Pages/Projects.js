89fa6204a493064ab7ca7df6d5440c28d094dbce
Aditya <ad47kumar@gmail.com>
2023-02-12 10:45:28 +0530

    project list showing grid wise

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,10 +1,23 @@
 import React from "react";
-
+import "../styles/Project.css"
+import ProjectItem from "../Components/ProjectItem";
+import { ProjectList } from "../helpers/ProjectList";
 const Project = () =>{
-    return <div>
-       
-        projects page
+    return (
+    <div className="projects">
+        <h1>My Projects</h1>
+        <div className="projectList">
+            {
+                ProjectList.map((project)=>{
+                    return <ProjectItem name={project.name} image={project.image}/>
+                })
+            }
+            
+
+        </div>
+                
     </div>
+    )
 }
 
 export default Project;
---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,22 @@
+import Pro1 from '../assets/proj1.jpg'
+import Pro2 from '../assets/proj2.jpg'
+import Pro5 from '../assets/proj5.jpg'
+
+export const ProjectList = [
+    {
+        name: "Project 1",
+        image: Pro1
+    },
+    {
+        name: "Project 2",
+        image: Pro2
+    },
+    {
+        name: "Project 3",
+        image: Pro5
+    },
+    {
+        name: "Project 3",
+        image: Pro5
+    }
+]
